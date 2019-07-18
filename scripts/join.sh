a2bbce3b9ec1ffd8a3228b097a084ba9296f314d
Chad Moon <chad.d.moon@gmail.com>
2019-07-18 12:00:36 -0600

    chmod

--- scripts/join.sh
+++ scripts/join.sh
@@ -1,1 +1,1 @@
-kubeadm join 10.32.229.91:6443 --token jz0mnq.9wj6u6n7r4nuq5em --discovery-token-ca-cert-hash sha256:4392fee7298de3448cb2426d73eb4e92f427c2254cb48028078d4d11550782c3
+kubeadm join 10.32.229.91:6443 --token pocea7.j5to2tniol6opwnc --discovery-token-ca-cert-hash sha256:f4f537fb74b7ce927746d2d41871ef966f56d1db1592165878a8bd933e90aaef
